c55f05adafe2333520cd45405bb0a3c2dc7b1268
Cameron Palk <cpalk@ix-trusty.cs.uoregon.edu>
2015-11-18 14:47:40 -0800

    Added homepage (index.php), might not need to be php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+    <title>Ticket Apprentice</title>
+    <link rel="stylesheet" type="text/css" href="global.css">
+    <link rel="stylesheet" type="text/css" href="homepage.css">
+</head>
+<body>
+<span id="customer">I am a customer</span>
+<span id="manager">I am a manager</span>
+</body>
+</html>
