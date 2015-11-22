fc39590c8a2456627824aadb2865391e3ff72b5b
Cameron Palk <ckpalk@comcast.net>
2015-11-22 14:47:05 -0800

    Started manager home, also added support for module design in global

---
+++ customer-home.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Customer Home</title>
+    <link rel="stylesheet" type="text/css" href="style/global.css">
+    <link rel="stylesheet" type="text/css" href="style/homepage.css">
+</head>
+<body>
+    Hello World.
+</body>
+</html>
---
+++ manager-home.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+    <title>Manager Home</title>
+    <link rel="stylesheet" type="text/css" href="style/global.css">
+    <link rel="stylesheet" type="text/css" href="style/manager.css">
+</head>
+<body>
+    <div id="menu_container">
+        <a href="_blank">link1</a>
+        <a href="_blank">link2</a>
+        <a href="_blank">link3</a>
+        <a href="_blank">link4</a>
+    </div>
+    <div id="body_container">
+    <div id="middle_container">
+        <div class="module">
+            <div class="module-title">Events</div>
+            <div class="module-body">
+            <ul>
+            <?php 
+                print "<li>Stuff1</li>";
+                print "<li>Stuff2</li>";
+                print "<li>Stuff3</li>";
+            ?>  
+            </ul>
+            </div>
+        </div>
+
+        <div class="module">
+            <div class="module-title">test</div>
+            <div class="module-body">
+                body
+            </div>
+        </div>
+
+    </div>
+    </div>
+</body>
+</html>
