85175ed3f314c2460a334a34030a8fe56fdfae72
Cameron Palk <ckpalk@comcast.net>
2015-11-23 14:51:13 -0800

    Added support for connecting to server on manager side, and a error page

---
+++ customer/customer-home.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <title>Customer Home</title>
+    <link rel="stylesheet" type="text/css" href="../support/global.css">
+    <link rel="stylesheet" type="text/css" href="../support/homepage.css">
+</head>
+<body>
+    Hello World.
+</body>
+</html>
